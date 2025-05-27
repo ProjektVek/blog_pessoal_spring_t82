a054e2c520d09a41f6f68e844654eeddd786eca1
Victor Hugo <projektvek@gmail.com>
2025-05-27 11:32:54 -0300

    :sparkles: Criada documentação swagger

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Usuario;
 import com.generation.blogpessoal.model.UsuarioLogin;
@@ -46,19 +48,21 @@ public class UsuarioService {
 		return Optional.ofNullable(usuarioRepository.save(usuario));
 	}
 	
-	public Optional<Usuario> atualizarUsuario(Usuario usuario){
-		
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+
 		if(usuarioRepository.findById(usuario.getId()).isPresent()) {
-			
-			Long usuarioPresenteId = usuarioRepository.findByUsuario(usuario.getUsuario()).get().getId();
-			if(usuarioPresenteId == usuario.getId()) {
-				usuario.setSenha(criptografarSenha(usuario.getSenha()));
-				
-				return Optional.ofNullable(usuarioRepository.save(usuario));
-			}
+
+			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
+
+			if ( (buscaUsuario.isPresent()) && ( buscaUsuario.get().getId() != usuario.getId()))
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+			return Optional.ofNullable(usuarioRepository.save(usuario));
 			
 		}
-		
+
 		return Optional.empty();
 	}
 	
